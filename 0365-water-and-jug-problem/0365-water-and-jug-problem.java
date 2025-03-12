d28cd0e7a52675c135ea67557f45bfdd3babaacb
sohailsheikh92 <108662082+sohailsheikh92@users.noreply.github.com>
2025-03-12 23:08:44 +0530

    Time: 0 ms (100%), Space: 40.2 MB (93.81%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+        if (jug1Capacity + jug2Capacity < targetCapacity) return false;
+        return targetCapacity % gcd(jug1Capacity, jug2Capacity) == 0;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
