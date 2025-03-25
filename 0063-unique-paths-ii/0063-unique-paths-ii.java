28b3cee7c05ee5fa041b1ba50b15be4ff098eb34
sohailsheikh92 <108662082+sohailsheikh92@users.noreply.github.com>
2025-03-25 11:49:35 +0530

    Time: 0 ms (100%), Space: 42.1 MB (28.25%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        
+        // If start or end is an obstacle, no paths exist
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) {
+            return 0;
+        }
+        
+        // Create DP array
+        int[][] dp = new int[m][n];
+        
+        // Initialize first cell
+        dp[0][0] = 1;
+        
+        // Initialize first row
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] == 0) {
+                dp[0][j] = dp[0][j-1];
+            }
+        }
+        
+        // Initialize first column
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 0) {
+                dp[i][0] = dp[i-1][0];
+            }
+        }
+        
+        // Fill the DP array
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
+    }
+}
