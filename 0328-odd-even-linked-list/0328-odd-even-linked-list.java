0d55f61223d00adef715a5f462916294d1063684
sohailsheikh92 <108662082+sohailsheikh92@users.noreply.github.com>
2025-06-11 10:28:59 +0530

    Time: 0 ms (100%), Space: 44.7 MB (50.61%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,43 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null) return head;
-
-        List<ListNode> oddList = new ArrayList<>();
-        List<ListNode> evenList = new ArrayList<>();
-
-        ListNode temp = head;
-        int idx = 1;
-
-        while (temp != null) {
-            if (idx % 2 != 0) {
-                oddList.add(temp);
-            } else {
-                evenList.add(temp);
-            }
-            temp = temp.next;
-            idx++;
+        ListNode odd=head;
+        if(odd==null || odd.next==null)
+            return head;
+        ListNode even=head.next;
+        ListNode evenStart=even;
+        while(odd.next!=null && even.next!=null){
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
         }
-
-        // Link all odd nodes
-        for (int i = 0; i < oddList.size() - 1; i++) {
-            oddList.get(i).next = oddList.get(i + 1);
-        }
-
-        // Link all even nodes
-        for (int i = 0; i < evenList.size() - 1; i++) {
-            evenList.get(i).next = evenList.get(i + 1);
-        }
-
-        // Connect end of odd list to start of even list
-        if (!oddList.isEmpty()) {
-            oddList.get(oddList.size() - 1).next = evenList.isEmpty() ? null : evenList.get(0);
-        }
-
-        // End the even list
-        if (!evenList.isEmpty()) {
-            evenList.get(evenList.size() - 1).next = null;
-        }
-
-        return oddList.get(0);
+        odd.next=evenStart;
+        return head;
     }
 }
