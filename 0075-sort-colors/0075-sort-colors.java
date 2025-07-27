eb94a3fff73db6a3721e9d1118f55666a4f4a807
sohailsheikh92 <108662082+sohailsheikh92@users.noreply.github.com>
2025-07-27 08:00:32 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.68%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,21 +3,19 @@ class Solution {
         int n=nums.length;
         int cnt0=0,cnt1=0,cnt2=0;
         for(int i : nums){
-            if(i==0)    
+            if(i==0)
                 cnt0++;
-            else if(i==1)
+            if(i==1)
                 cnt1++;
-            else 
+            if(i==2)
                 cnt2++;
         }
-        int i=0;
-        for(;i<cnt0;i++)
-            nums[i]=0;
-        int j=i;
-        for(;j<cnt0+cnt1;j++)
-            nums[j]=1;
-        int k=j;
-        for(;k<n;k++)
-            nums[k]=2;
+        int k=0;
+        for(int i=0;i<cnt0;i++)
+            nums[k++]=0;
+        for(int i=0;i<cnt1;i++)
+            nums[k++]=1;
+        for(int i=0;i<cnt2;i++)
+            nums[k++]=2;
     }
 }
