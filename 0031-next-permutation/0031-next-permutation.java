71bad741c1f574d0622e4b1ca834eae0b71dddd5
sohailsheikh92 <108662082+sohailsheikh92@users.noreply.github.com>
2025-08-28 19:57:33 +0530

    Time: 0 ms (100%), Space: 43.3 MB (21.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,47 +1,40 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-    int n = nums.length;
-    int idx = -1;
+        int n=nums.length;
+        int idx=-1;
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[i-1]){
+                // swap(nums,i,i-1);
+                idx=i-1;
+                break;
+            }
+        }
+        if(idx==-1){
+            rev(nums,0,n-1);
+            return ;
+        }
 
-    // Step 1: Find first decreasing element from right
-    for (int i = n - 1; i > 0; i--) {
-        if (nums[i] > nums[i - 1]) {
-            idx = i - 1;
-            break;
+        for(int i=n-1;i>idx;i--){
+            if(nums[i]>nums[idx]){
+                swap(nums,i,idx);
+                break;
+            }
+        }
+        rev(nums,idx+1,n-1);
+    }
+    static void swap(int nums[],int a,int b){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
+
+    static void rev(int nums[],int a,int b){
+        while(a<b){
+            int temp=nums[a];
+            nums[a]=nums[b];
+            nums[b]=temp;
+            a++;
+            b--;
         }
     }
-
-    // If entire array is in descending order, reverse it
-    if (idx == -1) {
-        rev(nums, 0, n - 1);
-        return;
-    }
-
-    // Step 2: Find element just larger than nums[idx]
-    for (int i = n - 1; i > idx; i--) {
-        if (nums[i] > nums[idx]) {
-            // Swap
-            int temp = nums[idx];
-            nums[idx] = nums[i];
-            nums[i] = temp;
-            break;
-        }
-    }
-
-    // Step 3: Reverse the suffix
-    rev(nums, idx + 1, n - 1);
-}
-
-// Helper to reverse part of array
-void rev(int[] nums, int l, int r) {
-    while (l < r) {
-        int temp = nums[l];
-        nums[l] = nums[r];
-        nums[r] = temp;
-        l++;
-        r--;
-    }
-}
-
-
 }
