2747f9764b66cc18f4dca41b1780ff2f0e6e01d9
sohailsheikh92 <108662082+sohailsheikh92@users.noreply.github.com>
2025-10-19 11:50:07 +0530

    Time: 14 ms (55.27%), Space: 45.6 MB (67.99%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,16 +1,34 @@
 class Solution {
     public String frequencySort(String s) {
-        Map<Character,Integer> map=new HashMap<>();
-        for(char ch :  s.toCharArray())
-            map.put(ch,map.getOrDefault(ch,0)+1);
-        
-        List<Map.Entry<Character,Integer>> li=new ArrayList<>(map.entrySet());
+        HashMap<Character, Integer> map = new HashMap<>();
 
-        li.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-        StringBuilder sb=new StringBuilder();
-        for(Map.Entry<Character,Integer> entry : li){
-            sb.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+        // Step 1: Count frequency of each character
+        for (char ch : s.toCharArray())
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+        StringBuilder sb = new StringBuilder();
+
+        // Step 2: Build result by repeatedly picking max frequency
+        while (!map.isEmpty()) {
+            int max = Integer.MIN_VALUE;
+            char maxChar = 0; // to store which character has max frequency
+
+            // find character with highest frequency
+            for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+                if (entry.getValue() > max) {
+                    max = entry.getValue();
+                    maxChar = entry.getKey();
+                }
+            }
+
+            // append that character 'max' times
+            for (int i = 0; i < max; i++)
+                sb.append(maxChar);
+
+            // remove that character from the map
+            map.remove(maxChar);
         }
+
         return sb.toString();
     }
 }
