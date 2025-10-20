e4a35845c35c182a6ade0020f6e726162ab04cc0
sohailsheikh92 <108662082+sohailsheikh92@users.noreply.github.com>
2025-10-20 15:15:46 +0530

    Time: 1661 ms (11.1%), Space: 42.1 MB (87.4%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,31 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";  // If empty, return ""
-
-        int start = 0, maxLen = 0;  // Start index & max length of longest palindrome
-
-        for (int i = 0; i < s.length(); i++) {  
-            // Expand around a single character (odd-length palindrome)
-            int len1 = expandAroundCenter(s, i, i);
-            // Expand between two characters (even-length palindrome)
-            int len2 = expandAroundCenter(s, i, i + 1);
-            
-            int len = Math.max(len1, len2);  // Take the longest found
-            
-            if (len > maxLen) {  // If it's the longest so far, update start & maxLen
-                maxLen = len;
-                start = i - (len - 1) / 2;  // Calculate the starting index of the palindrome
+        int max=0,start=0,end=0,n=s.length();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                if(isPali(s,i,j)){
+                    if(max<j-i+1){
+                        max=j-i+1;
+                        start=i;
+                        end=j;
+                    }
+                }
             }
         }
-
-        return s.substring(start, start + maxLen);  // Extract the longest palindrome
+        return s.substring(start,end+1);
     }
 
-    private int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;  // Move left pointer left
-            right++; // Move right pointer right
+    static boolean isPali(String s,int i,int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j))
+                return false;
+            i++;
+            j--;
         }
-        return right - left - 1;  // Length of the palindrome
+        return true;
     }
 }
